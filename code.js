07ab3c5413dc8bd030a0d4b72139ecbe64bdda73
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-15 15:36:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,11 @@
 function convertToAdjList(adjMatrix) {
-    adjMatrix = [];
     let list = [];
     for (let i= 0; i < adjMatrix.length; i++){
-        adjmatrix[i] = [];
+        list[i] = [];
         for(let j = 0; j < adjMatrix[i].length; j++) {
-                adjmatrix[i][j] = 0;
-        }
-        if(adjmatrix[i][j] === 1){
-            list[i].push(adjmatrix[i][j];
+            if(adjMatrix[i][j] === 1){
+                list[i].push(j);
+            }
         }
     }
     return list;
